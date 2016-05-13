fc7af700a7972fc38e2fddfaad8d84cd1acd9e3c
Jay Johnson <jayevanjohnson@gmail.com>
2016-05-12 19:10:54 -0700

    Create default and template pages

--- wp-content/themes/es-wp-foundation/template-default.php
+++ wp-content/themes/es-wp-foundation/template-default.php
@@ -1,3 +1,8 @@
+<?php
+/*
+  Template Name: Template-Default
+ */
+?>
 <?php get_header(); ?>
   <body>
     <div class="top-bar">
@@ -44,45 +49,23 @@
         <hr>
         <div class="row">
             <div class="medium-4 columns">
+                <img class="thumbnail" src="http://placehold.it/360x200">
                 <h3>Headline</h3>
                 <p>Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed nec diam eu diam mattis viverra. Nulla fringilla, orci ac euismod semper, magna.</p>
             </div>
             <div class="medium-4 columns">
+                <img class="thumbnail" src="http://placehold.it/360x200">
                 <h3>Headline</h3>
                 <p>Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed nec diam eu diam mattis viverra. Nulla fringilla, orci ac euismod semper, magna.</p>
             </div>
             <div class="medium-4 columns">
+                <img class="thumbnail" src="http://placehold.it/360x200">
                 <h3>Headline</h3>
                 <p>Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed nec diam eu diam mattis viverra. Nulla fringilla, orci ac euismod semper, magna.</p>
             </div>
         </div>
         <hr>
         <div class="row column">
-            <ul class="vertical medium-horizontal menu expanded text-center">
-                <li>
-                    <a href="#">
-                        <div class="stat">28</div><span>Visitor</span></a>
-                </li>
-                <li>
-                    <a href="#">
-                        <div class="stat">43</div><span>Services</span></a>
-                </li>
-                <li>
-                    <a href="#">
-                        <div class="stat">95</div><span>Resident</span></a>
-                </li>
-                <li>
-                    <a href="#">
-                        <div class="stat">59</div><span>Business</span></a>
-                </li>
-                <li>
-                    <a href="#">
-                        <div class="stat">18</div><span>Government</span></a>
-                </li>
-            </ul>
-        </div>
-        <div class="row column">
-            <hr>
             <ul class="menu footer-menu">
                 <li class="float-left"><a href="#">Visitor</a></li>
                 <li class="float-left"><a href="#">Services</a></li>
---
+++ wp-content/themes/es-wp-foundation/template-services.php
@@ -0,0 +1,171 @@
+<?php
+/*
+  Template Name: Template-Services
+ */
+?>
+<?php get_header(); ?>
+  <body>
+    <div class="top-bar">
+        <div class="row">
+            <div class="top-bar-left">
+                <ul class="dropdown menu" data-dropdown-menu>
+                    <li class="menu-text">The City of El Segundo</li>
+                    <li><a href="#">Visitor</a></li>
+                    <li><a href="#">Services</a></li>
+                    <li><a href="#">Resident</a></li>
+                    <li><a href="#">Business</a></li>
+                    <li><a href="#">Government</a></li>
+                </ul>
+            </div>
+            <div class="top-bar-right">
+                <ul class="menu">
+                    <li>
+                        <input type="search" placeholder="Search">
+                    </li>
+                    <li>
+                        <button type="button" class="button">Search</button>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <br>
+        <div class="row">
+            <div class="medium-6 columns">
+                <img class="thumbnail" src="http://placehold.it/650x350">
+            </div>
+            <div class="medium-6 columns">
+                <h3>Headline</h3>
+                <p>Nunc eu ullamcorper orci. Quisque eget odio ac lectus vestibulum faucibus eget in metus. In pellentesque faucibus vestibulum. Nulla at nulla justo, eget luctus tortor. Nulla facilisi. Duis aliquet egestas purus in.</p>
+            </div>
+        </div>
+        <hr>
+        <div class="row">
+            <div class="medium-4 columns">
+                <div class="media-object">
+                    <div class="media-object-section">
+                        <img class="thumbnail" src="http://placehold.it/100x100">
+                    </div>
+                    <div class="media-object-section">
+                        <h5>Headline</h5>
+                        <p>Quisque eget odio ac lectus vestibulum faucibus eget in metus. In pellentesque.</p>
+                    </div>
+                </div>
+                <div class="media-object">
+                    <div class="media-object-section">
+                        <img class="thumbnail" src="http://placehold.it/100x100">
+                    </div>
+                    <div class="media-object-section">
+                        <h5>Headline</h5>
+                        <p>Quisque eget odio ac lectus vestibulum faucibus eget in metus. In pellentesque.</p>
+                    </div>
+                </div>
+                <div class="media-object">
+                    <div class="media-object-section">
+                        <img class="thumbnail" src="http://placehold.it/100x100">
+                    </div>
+                    <div class="media-object-section">
+                        <h5>Headline</h5>
+                        <p>Quisque eget odio ac lectus vestibulum faucibus eget in metus. In pellentesque.</p>
+                    </div>
+                </div>
+                <div class="media-object">
+                    <div class="media-object-section">
+                        <img class="thumbnail" src="http://placehold.it/100x100">
+                    </div>
+                    <div class="media-object-section">
+                        <h5>Headline</h5>
+                        <p>Quisque eget odio ac lectus vestibulum faucibus eget in metus. In pellentesque.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="medium-4 columns">
+                <div class="media-object">
+                    <div class="media-object-section">
+                        <img class="thumbnail" src="http://placehold.it/100x100">
+                    </div>
+                    <div class="media-object-section">
+                        <h5>Headline</h5>
+                        <p>Quisque eget odio ac lectus vestibulum faucibus eget in metus. In pellentesque.</p>
+                    </div>
+                </div>
+                <div class="media-object">
+                    <div class="media-object-section">
+                        <img class="thumbnail" src="http://placehold.it/100x100">
+                    </div>
+                    <div class="media-object-section">
+                        <h5>Headline</h5>
+                        <p>Quisque eget odio ac lectus vestibulum faucibus eget in metus. In pellentesque.</p>
+                    </div>
+                </div>
+                <div class="media-object">
+                    <div class="media-object-section">
+                        <img class="thumbnail" src="http://placehold.it/100x100">
+                    </div>
+                    <div class="media-object-section">
+                        <h5>Headline</h5>
+                        <p>Quisque eget odio ac lectus vestibulum faucibus eget in metus. In pellentesque.</p>
+                    </div>
+                </div>
+                <div class="media-object">
+                    <div class="media-object-section">
+                        <img class="thumbnail" src="http://placehold.it/100x100">
+                    </div>
+                    <div class="media-object-section">
+                        <h5>Headline</h5>
+                        <p>Quisque eget odio ac lectus vestibulum faucibus eget in metus. In pellentesque.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="medium-4 columns">
+                <div class="media-object">
+                    <div class="media-object-section">
+                        <img class="thumbnail" src="http://placehold.it/100x100">
+                    </div>
+                    <div class="media-object-section">
+                        <h5>Headline</h5>
+                        <p>Quisque eget odio ac lectus vestibulum faucibus eget in metus. In pellentesque.</p>
+                    </div>
+                </div>
+                <div class="media-object">
+                    <div class="media-object-section">
+                        <img class="thumbnail" src="http://placehold.it/100x100">
+                    </div>
+                    <div class="media-object-section">
+                        <h5>Headline</h5>
+                        <p>Quisque eget odio ac lectus vestibulum faucibus eget in metus. In pellentesque.</p>
+                    </div>
+                </div>
+                <div class="media-object">
+                    <div class="media-object-section">
+                        <img class="thumbnail" src="http://placehold.it/100x100">
+                    </div>
+                    <div class="media-object-section">
+                        <h5>Headline</h5>
+                        <p>Quisque eget odio ac lectus vestibulum faucibus eget in metus. In pellentesque.</p>
+                    </div>
+                </div>
+                <div class="media-object">
+                    <div class="media-object-section">
+                        <img class="thumbnail" src="http://placehold.it/100x100">
+                    </div>
+                    <div class="media-object-section">
+                        <h5>Headline</h5>
+                        <p>Quisque eget odio ac lectus vestibulum faucibus eget in metus. In pellentesque.</p>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+        <div class="row column">
+            <hr>
+            <ul class="menu footer-menu">
+                <li class="float-left"><a href="#">Visitor</a></li>
+                <li class="float-left"><a href="#">Services</a></li>
+                <li class="float-left"><a href="#">Resident</a></li>
+                <li class="float-left"><a href="#">Business</a></li>
+                <li class="float-left"><a href="#">Government</a></li>
+                <li class="float-right"><a href="#">&copy; 2016. The City of El Segundo</a></li>
+            </ul>
+        </div>
+    <?php get_footer(); ?>
