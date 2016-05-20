5b857590def59566f6c4196aaeb5f4eec460f9fd
Jay Johnson <jayevanjohnson@gmail.com>
2016-05-20 14:50:33 -0700

    Add Gulp setup to expedite site development

--- wp-content/themes/es-wp-foundation/functions.php
+++ wp-content/themes/es-wp-foundation/functions.php
@@ -1,7 +1,7 @@
 <?php
 function register_my_styles(){
-  wp_register_style('foundation', get_template_directory_uri()."/css/foundation.min.css",array(),'5.1.1','screen');
-  wp_register_style('main', get_template_directory_uri()."/css/app.css",array(),'2.7','all');
+  wp_register_style('foundation', get_template_directory_uri()."/css/foundation-test.css",array(),'5.1.1','screen');
+  wp_register_style('main', get_template_directory_uri()."/style.css",array(),'2.7','all');
 
   wp_enqueue_style(array('foundation','main'));
 }
@@ -10,7 +10,7 @@ add_action('wp_enqueue_scripts', 'register_my_styles');
 
 function register_my_scripts() {
   wp_register_script('jquery', "/js/vendor/jquery.js", array(),'2.1.0',false);
-  wp_register_script('foundation-js', "http://dhbhdrzi4tiry.cloudfront.net/cdn/sites/foundation.js", array(),'2.1.0',false);
+  wp_register_script('foundation-js', "/js/vendor/foundation.js", array(),'2.1.0',false);
 
   wp_enqueue_script(array('jquery','foundation-js'));
 }
