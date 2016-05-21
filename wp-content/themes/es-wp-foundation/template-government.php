61c641397989fdf58de50a45198cca088c1d75f3
Jay Johnson <jayevanjohnson@gmail.com>
2016-05-21 16:52:07 -0700

    Create and populate government page template

---
+++ wp-content/themes/es-wp-foundation/template-government.php
@@ -0,0 +1,137 @@
+<?php
+/*
+  Template Name: Template-Government
+ */
+?>
+<?php get_header(); ?>
+
+    <br>
+        <div class="row">
+            <div class="medium-6 columns">
+                <img class="thumbnail" src="<?php bloginfo('template_url'); ?>/assets/src/img/es-city-hall.jpg">
+            </div>
+            <div class="medium-6 columns">
+                <h3>Government</h3>
+                <p>City Services including: <a href="#">City Clerk,</a> <a href="#">City Manager's Office,</a> <a href="#">City Treasurer,</a> <a href="#">Elected Officials,</a> <a href="#">Finance,</a> <a href="#">Fire Department,</a> <a href="#">Human Resources,</a> <a href="#">Library,</a> <a href="#">Planning and Building Safety,</a> <a href="#">Police Department,</a> <a href="#">Public Works,</a> <a href="#">Recreation and Parks</a></p>
+            </div>
+        </div>
+        <hr>
+        <div class="row">
+            <div class="medium-4 columns">
+                <div class="media-object">
+                    <div class="media-object-section">
+                        <img class="thumbnail" src="http://placehold.it/100x100">
+                    </div>
+                    <div class="media-object-section">
+                        <h5><a href="#">City Clerk</a></h5>
+                        <p><a href="#">Frequently Asked Questions,</a> <a href="#">Services,</a> <a href="#">Available Documents &amp; Forms,</a> <a href="#">Divisions,</a> <a href="#">Contact List</a> </p>
+                    </div>
+                </div>
+                <div class="media-object">
+                    <div class="media-object-section">
+                        <img class="thumbnail" src="http://placehold.it/100x100">
+                    </div>
+                    <div class="media-object-section">
+                        <h5><a href="#">City Manager's Office</a></h5>
+                        <p><a href="#">City Manager,</a> <a href="#">City Attorney,</a> <a href="#">City Manager Senior E/A,</a> <a href="#">City Council E/A,</a> <a href="#">Contacts</a></p>
+                    </div>
+                </div>
+                <div class="media-object">
+                    <div class="media-object-section">
+                        <img class="thumbnail" src="http://placehold.it/100x100">
+                    </div>
+                    <div class="media-object-section">
+                        <h5><a href="#">City Treasurer</a></h5>
+                        <p><a href="#">Treasurer,</a> <a href="#">Department Highlights,</a> <a href="#">Goals and Objectives,</a> <a href="#">Reports, Press Releases</a></p>
+                    </div>
+                </div>
+                <div class="media-object">
+                    <div class="media-object-section">
+                        <img class="thumbnail" src="http://placehold.it/100x100">
+                    </div>
+                    <div class="media-object-section">
+                        <h5><a href="#">Elected Officials</a></h5>
+                        <p><a href="#">Council,</a> <a href="#">City Clerk,</a> <a href="#">City Treasurer,</a> <a href="#">Contacts</a></p>
+                    </div>
+                </div>
+            </div>
+            <div class="medium-4 columns">
+                <div class="media-object">
+                    <div class="media-object-section">
+                        <img class="thumbnail" src="http://placehold.it/100x100">
+                    </div>
+                    <div class="media-object-section">
+                        <h5><a href="#">Finance</a></h5>
+                        <p><a href="#">Budgets,</a> <a href="#">Comprehensive Annual Financial Reports</a></p>
+                    </div>
+                </div>
+                <div class="media-object">
+                    <div class="media-object-section">
+                        <img class="thumbnail" src="http://placehold.it/100x100">
+                    </div>
+                    <div class="media-object-section">
+                        <h5><a href="#">Fire Department</a></h5>
+                        <p><a href="#">Fire Stations,</a> <a href="#">Paramedics,</a> <a href="#">Fire Prevention,</a> <a href="#">Environmental Safety,</a> <a href="#">Sandbag Distribution,</a> <a href="#">Services</a></p>
+                    </div>
+                </div>
+                <div class="media-object">
+                    <div class="media-object-section">
+                        <img class="thumbnail" src="http://placehold.it/100x100">
+                    </div>
+                    <div class="media-object-section">
+                        <h5><a href="#">Human Resources</a></h5>
+                        <p><a href="#">Positions Available,</a> <a href="#">Part-Time &amp; Promotional Opportunities,</a> <a href="#">Contacts,</a> <a href="#">Mission Statement</a></p>
+                    </div>
+                </div>
+                <div class="media-object">
+                    <div class="media-object-section">
+                        <img class="thumbnail" src="http://placehold.it/100x100">
+                    </div>
+                    <div class="media-object-section">
+                        <h5><a href="#">Library</a></h5>
+                        <p><a href="#">Operating Hours,</a> <a href="#">Events,</a> <a href="#">History,</a> <a href="#">Services,</a> <a href="#">Calendar</a></p>
+                    </div>
+                </div>
+            </div>
+            <div class="medium-4 columns">
+                <div class="media-object">
+                    <div class="media-object-section">
+                        <img class="thumbnail" src="http://placehold.it/100x100">
+                    </div>
+                    <div class="media-object-section">
+                        <h5><a href="#">Planning &amp; Building Safety</a></h5>
+                        <p><a href="#">Appointment Plan Check,</a> <a href="#">Permits,</a> <a href="#">Case Planner Process,</a> <a href="#">Inspections,</a> <a href="#">Municipal Code</a></p>
+                    </div>
+                </div>
+                <div class="media-object">
+                    <div class="media-object-section">
+                        <img class="thumbnail" src="http://placehold.it/100x100">
+                    </div>
+                    <div class="media-object-section">
+                        <h5><a href="#">Police Department</a></h5>
+                        <p><a href="#">Vision,</a> <a href="#">Mission,</a> <a href="#">Values,</a> <a href="#">Goals &amp; Objectives,</a> <a href="#">Area Command Program,</a> <a href="#">Community Relations</a></p>
+                    </div>
+                </div>
+                <div class="media-object">
+                    <div class="media-object-section">
+                        <img class="thumbnail" src="http://placehold.it/100x100">
+                    </div>
+                    <div class="media-object-section">
+                        <h5><a href="#">Public Works</a></h5>
+                        <p><a href="#">Frequently Asked Questions,</a> <a href="#">Services,</a> <a href="#">Divisions,</a> <a href="#">Documents,</a> <a href="#">Contact</a></p>
+                    </div>
+                </div>
+                <div class="media-object">
+                    <div class="media-object-section">
+                        <img class="thumbnail" src="http://placehold.it/100x100">
+                    </div>
+                    <div class="media-object-section">
+                        <h5><a href="#">Recreation &amp; Parks</a></h5>
+                        <p><a href="#">Programs,</a> <a href="#">Events,</a> <a href="#">Registration,</a> <a href="#">Parks Commission,</a> <a href="#">Sponsorship Opportunities</a></p>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+    <?php get_footer(); ?>
