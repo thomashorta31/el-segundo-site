4d87cf3fa3d17b13efd7c4654b49759163655c95
Jay Johnson <jayevanjohnson@gmail.com>
2016-05-29 18:29:12 -0700

    Create page templates for each government service

--- wp-content/themes/es-wp-foundation/template-government.php
+++ wp-content/themes/es-wp-foundation/template-government.php
@@ -30,28 +30,28 @@
                 </div>
                 <div class="media-object">
                     <div class="media-object-section">
-                        <a href="/index.php?page_id=51"><h2 class="fontglyph">B</h2></a>
+                        <a href="/index.php?page_id=55"><h2 class="fontglyph">B</h2></a>
                     </div>
                     <div class="media-object-section">
-                        <h5><a href="#">City Manager's Office</a></h5>
+                        <h5><a href="/index.php?page_id=55">City Manager's Office</a></h5>
                         <p>City Manager, City Attorney, City Manager Senior E/A, City Council E/A</p>
                     </div>
                 </div>
                 <div class="media-object">
                     <div class="media-object-section">
-                        <a href="/index.php?page_id=51"><h2 class="fontglyph">C</h2></a>
+                        <a href="/index.php?page_id=57"><h2 class="fontglyph">C</h2></a>
                     </div>
                     <div class="media-object-section">
-                        <h5><a href="#">City Treasurer</a></h5>
+                        <h5><a href="/index.php?page_id=57">City Treasurer</a></h5>
                         <p>Treasurer, Department Highlights, Goals and Objectives, Reports, Press Releases</p>
                     </div>
                 </div>
                 <div class="media-object">
                     <div class="media-object-section">
-                        <a href="/index.php?page_id=51"><h2 class="fontglyph">D</h2></a>
+                        <a href="/index.php?page_id=59"><h2 class="fontglyph">D</h2></a>
                     </div>
                     <div class="media-object-section">
-                        <h5><a href="#">Elected Officials</a></h5>
+                        <h5><a href="/index.php?page_id=59">Elected Officials</a></h5>
                         <p>Council, City Clerk, City Treasurer</p>
                     </div>
                 </div>
@@ -59,37 +59,37 @@
             <div class="medium-4 columns">
                 <div class="media-object">
                     <div class="media-object-section">
-                        <a href="/index.php?page_id=51"><h2 class="fontglyph">E</h2></a>
+                        <a href="/index.php?page_id=61"><h2 class="fontglyph">E</h2></a>
                     </div>
                     <div class="media-object-section">
-                        <h5><a href="#">Finance</a></h5>
+                        <h5><a href="/index.php?page_id=61">Finance</a></h5>
                         <p>Budgets,</a> Comprehensive Annual Financial Reports</a></p>
                     </div>
                 </div>
                 <div class="media-object">
                     <div class="media-object-section">
-                        <a href="/index.php?page_id=51"><h2 class="fontglyph">F</h2></a>
+                        <a href="/index.php?page_id=63"><h2 class="fontglyph">F</h2></a>
                     </div>
                     <div class="media-object-section">
-                        <h5><a href="#">Fire Department</a></h5>
+                        <h5><a href="/index.php?page_id=63">Fire Department</a></h5>
                         <p>Fire Stations, Paramedics, Fire Prevention, Environmental Safety, Sandbag Distribution, Services</p>
                     </div>
                 </div>
                 <div class="media-object">
                     <div class="media-object-section">
-                        <a href="/index.php?page_id=51"><h2 class="fontglyph">G</h2></a>
+                        <a href="/index.php?page_id=65"><h2 class="fontglyph">G</h2></a>
                     </div>
                     <div class="media-object-section">
-                        <h5><a href="#">Human Resources</a></h5>
+                        <h5><a href="/index.php?page_id=65">Human Resources</a></h5>
                         <p>Positions Available, Part-Time &amp; Promotional Opportunities, Contacts, Mission Statement</p>
                     </div>
                 </div>
                 <div class="media-object">
                     <div class="media-object-section">
-                        <a href="/index.php?page_id=51"><h2 class="fontglyph">H</h2></a>
+                        <a href="/index.php?page_id=67"><h2 class="fontglyph">H</h2></a>
                     </div>
                     <div class="media-object-section">
-                        <h5><a href="#">Library</a></h5>
+                        <h5><a href="/index.php?page_id=67">Library</a></h5>
                         <p>Operating Hours, Events, History, Services, Calendar</p>
                     </div>
                 </div>
@@ -97,37 +97,37 @@
             <div class="medium-4 columns">
                 <div class="media-object">
                     <div class="media-object-section">
-                        <a href="/index.php?page_id=51"><h2 class="fontglyph">I</h2></a>
+                        <a href="/index.php?page_id=69"><h2 class="fontglyph">I</h2></a>
                     </div>
                     <div class="media-object-section">
-                        <h5><a href="#">Planning &amp; Building Safety</a></h5>
+                        <h5><a href="/index.php?page_id=69">Planning &amp; Building Safety</a></h5>
                         <p>Appointment Plan Check, Permits, Case Planner Process, Inspections, Municipal Code</p>
                     </div>
                 </div>
                 <div class="media-object">
                     <div class="media-object-section">
-                        <a href="/index.php?page_id=51"><h2 class="fontglyph">J</h2></a>
+                        <a href="/index.php?page_id=71"><h2 class="fontglyph">J</h2></a>
                     </div>
                     <div class="media-object-section">
-                        <h5><a href="#">Police Department</a></h5>
+                        <h5><a href="/index.php?page_id=71">Police Department</a></h5>
                         <p>Vision, Mission, Values, Goals &amp; Objectives, Area Command Program, Community Relations</p>
                     </div>
                 </div>
                 <div class="media-object">
                     <div class="media-object-section">
-                        <a href="/index.php?page_id=51"><h2 class="fontglyph">K</h2></a>
+                        <a href="/index.php?page_id=73"><h2 class="fontglyph">K</h2></a>
                     </div>
                     <div class="media-object-section">
-                        <h5><a href="#">Public Works</a></h5>
+                        <h5><a href="/index.php?page_id=73">Public Works</a></h5>
                         <p>Frequently Asked Questions, Services, Divisions, Documents</p>
                     </div>
                 </div>
                 <div class="media-object">
                     <div class="media-object-section">
-                        <a href="/index.php?page_id=51"><h2 class="fontglyph">L</h2></a>
+                        <a href="/index.php?page_id=82"><h2 class="fontglyph">L</h2></a>
                     </div>
                     <div class="media-object-section">
-                        <h5><a href="#">Recreation &amp; Parks</a></h5>
+                        <h5><a href="/index.php?page_id=82">Recreation &amp; Parks</a></h5>
                         <p>Programs, Events, Registration, Parks Commission, Sponsorship Opportunities</p>
                     </div>
                 </div>
