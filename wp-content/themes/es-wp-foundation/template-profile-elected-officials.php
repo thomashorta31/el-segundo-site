5903d52c6de4b929e467474836d2ca704e5de2ef
Jay Johnson <jayevanjohnson@gmail.com>
2016-06-02 23:11:50 -0700

    Add live content to elected officials template

--- wp-content/themes/es-wp-foundation/template-profile-elected-officials.php
+++ wp-content/themes/es-wp-foundation/template-profile-elected-officials.php
@@ -12,70 +12,128 @@
                 <h2 class="fontglyph inactive">D</h2>
             </div>
             <div class="medium-11 columns">
-                <h3 class="title-down">Elected Officials: <span style='font-weight: normal;'>Subhead</span></h3>
+                <h3 class="title-down">Elected Officials</h3>
             </div>
-        </div>
+        </div><!--====  End of .row  ====-->
         <hr>
         <div class="row">
             <div class="medium-12 columns">
                 <div class="media-object">
                     <div class="media-object-section">
-                        <h5><a href="#">Contact</a></h5>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est at, libero asperiores, itaque fuga sed dicta quos modi excepturi eligendi laborum nesciunt voluptatem odio. Nihil commodi voluptates eaque, repellendus odit?</p>
+                        <h5>Contact</h5>
+                        <p>
+                            <a href="mailto:allcouncilandclerks@elsegundo.org?Subject=Hello%20Councilmembers" target="_top">allcouncilandclerks@elsegundo.org</a></br>
+                            <a href="tel:+1-310-524-2302">(310) 524-2302</a></br>
+                            350 Main Street, El Segundo, CA 90245</br>
+                            Monday - Thursday 7:00 a.m. - 6:00 p.m. (closed on Friday)</br>
+                        </p>
                     </div>
                 </div>
             </div>
-        </div>
+        </div><!--====  End of .row  ====-->
         <hr>
         <div class="row">
-            <div class="medium-4 columns">
+            <div class="medium-6 columns">
                 <div class="media-object">
                     <div class="media-object-section">
-                        <h5><a href="#">Subhead</a></h5>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est at, libero asperiores, itaque fuga sed dicta quos modi excepturi eligendi laborum nesciunt voluptatem odio. Nihil commodi voluptates eaque, repellendus odit?</p>
+                        <h5>Documents &amp; Information</h5>
+                        <p>
+                            <a href="http://www.elsegundo.org/civicax/filebank/blobdload.aspx?blobid=4950" target="_blank">Application to Committees/Commissions/Boards</a></br>
+                            <a href="http://www.elsegundo.org/civicax/filebank/blobdload.aspx?BlobID=14166" target="_blank">Local Appointments List</a></br>
+                        </p>
                     </div>
                 </div>
             </div>
-            <div class="medium-4 columns">
+            <div class="medium-6 columns">
                 <div class="media-object">
                     <div class="media-object-section">
-                        <h5><a href="#">Subhead</a></h5>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est at, libero asperiores, itaque fuga sed dicta quos modi excepturi eligendi laborum nesciunt voluptatem odio. Nihil commodi voluptates eaque, repellendus odit?</p>
+                        <h5>Mission</h5>
+                        <p>
+                            City Council members establish policy for the City. The City Manager and staff are then responsible for implementing the policy. The City Council adopts an annual budget that establishes City services and service levels; establishes tax rates, license fees, assessments, franchises and other forms of revenue as set forth by Government Code; direct the development of the City by adopting a General Plan and supporting the Zoning Code; authorize contracts and leases, and disposes of City real and personal property.
+                        </p>
                     </div>
                 </div>
             </div>
-            <div class="medium-4 columns">
+        </div><!--====  End of .row  ====-->
+        <hr>
+        <div class="row">
+            <div class="medium-3 columns">
                 <div class="media-object">
                     <div class="media-object-section">
-                        <h5><a href="#">Subhead</a></h5>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est at, libero asperiores, itaque fuga sed dicta quos modi excepturi eligendi laborum nesciunt voluptatem odio. Nihil commodi voluptates eaque, repellendus odit?</p>
+                        <h5>Mayor: </br><span style='font-weight: normal;'>Suzanne Fuentes</span></h5>
+                        <p>
+                            <a href="mailto:sfuentes@elsegundo.org?Subject=Hello%20Suzanne" target="_top">sfuentes@elsegundo.org</a></br>
+                            <a href="tel:+1-310-524-2302">(310) 524-2302</a></br>
+                        </p>
                     </div>
                 </div>
             </div>
-            <div class="medium-4 columns">
+            <div class="medium-3 columns">
                 <div class="media-object">
                     <div class="media-object-section">
-                        <h5><a href="#">Subhead</a></h5>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est at, libero asperiores, itaque fuga sed dicta quos modi excepturi eligendi laborum nesciunt voluptatem odio. Nihil commodi voluptates eaque, repellendus odit?</p>
+                        <h5>Mayor Pro Tem: </br><span style='font-weight: normal;'>Drew Boyles</span></h5>
+                        <p>
+                            <a href="mailto:dboyles@elsegundo.org?Subject=Hello%20Drew" target="_top">dboyles@elsegundo.org</a></br>
+                            <a href="tel:+1-310-524-2302">(310) 524-2302</a></br>
+                        </p>
                     </div>
                 </div>
             </div>
-            <div class="medium-4 columns">
+            <div class="medium-3 columns">
                 <div class="media-object">
                     <div class="media-object-section">
-                        <h5><a href="#">Subhead</a></h5>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est at, libero asperiores, itaque fuga sed dicta quos modi excepturi eligendi laborum nesciunt voluptatem odio. Nihil commodi voluptates eaque, repellendus odit?</p>
+                        <h5>Council Member: </br><span style='font-weight: normal;'>Don Brann</span></h5>
+                        <p>
+                            <a href="mailto:dbrann@elsegundo.org?Subject=Hello%20Don" target="_top">dbrann@elsegundo.org</a></br>
+                            <a href="tel:+1-310-524-2302">(310) 524-2302</a></br>
+                        </p>
                     </div>
                 </div>
             </div>
-            <div class="medium-4 columns">
+            <div class="medium-3 columns">
                 <div class="media-object">
                     <div class="media-object-section">
-                        <h5><a href="#">Subhead</a></h5>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est at, libero asperiores, itaque fuga sed dicta quos modi excepturi eligendi laborum nesciunt voluptatem odio. Nihil commodi voluptates eaque, repellendus odit?</p>
+                        <h5>Council Member: </br><span style='font-weight: normal;'>Carol Pirsztuk</span></h5>
+                        <p>
+                            <a href="mailto:cpirsztuk@elsegundo.org?Subject=Hello%20Carol" target="_top">cpirsztuk@elsegundo.org</a></br>
+                            <a href="tel:+1-310-524-2302">(310) 524-2302</a></br>
+                        </p>
                     </div>
                 </div>
             </div>
-        </div>
+            <div class="medium-3 columns">
+                <div class="media-object">
+                    <div class="media-object-section">
+                        <h5>Council Member: </br><span style='font-weight: normal;'>Michael Dugan</span></h5>
+                        <p>
+                            <a href="mailto:mdugan@elsegundo.org?Subject=Hello%20Michael" target="_top">mdugan@elsegundo.org</a></br>
+                            <a href="tel:+1-310-524-2302">(310) 524-2302</a></br>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="medium-3 columns">
+                <div class="media-object">
+                    <div class="media-object-section">
+                        <h5>City Clerk: </br><span style='font-weight: normal;'>Tracy Weaver</span></h5>
+                        <p>
+                            <a href="mailto:tweaver@elsegundo.org?Subject=Hello%20Tracy" target="_top">tweaver@elsegundo.org</a></br>
+                            <a href="tel:+1-310-524-2305">(310) 524-2305</a></br>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="medium-3 columns">
+                <div class="media-object">
+                    <div class="media-object-section">
+                        <h5>City Treasurer: </br><span style='font-weight: normal;'>Crista Binder</span></h5>
+                        <p>
+                            <a href="mailto:cbinder@elsegundo.org?Subject=Hello%20Crista" target="_top">cbinder@elsegundo.org</a></br>
+                            <a href="tel:+1-310-524-2309">(310) 524-2309</a></br>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div><!--====  End of .row  ====-->
 
     <?php get_footer(); ?>
