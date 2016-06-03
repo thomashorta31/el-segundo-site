01e494569645768fadc71336206396663ee6f94c
Jay Johnson <jayevanjohnson@gmail.com>
2016-06-03 10:44:35 -0700

    Add live content to human resources template

--- wp-content/themes/es-wp-foundation/template-profile-human-resources.php
+++ wp-content/themes/es-wp-foundation/template-profile-human-resources.php
@@ -12,70 +12,74 @@
                 <h2 class="fontglyph inactive">G</h2>
             </div>
             <div class="medium-11 columns">
-                <h3 class="title-down">Human Resources: <span style='font-weight: normal;'>Subhead</span></h3>
+                <h3 class="title-down">Human Resources</h3>
             </div>
-        </div>
+        </div> <!--====  End of .row  ====-->
         <hr>
         <div class="row">
             <div class="medium-12 columns">
                 <div class="media-object">
                     <div class="media-object-section">
-                        <h5><a href="#">Contact</a></h5>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est at, libero asperiores, itaque fuga sed dicta quos modi excepturi eligendi laborum nesciunt voluptatem odio. Nihil commodi voluptates eaque, repellendus odit?</p>
+                        <h5>Contact</h5>
+                        <p>
+                            <a href="mailto:hr@elsegundo.org?Subject=Hello%20Tracy" target="_top">hr@elsegundo.org</a></br>
+                            <a href="tel:+1-310-524-2381">(310) 524-2381</a></br>
+                            350 Main Street, El Segundo, CA 90245</br>
+                        </p>
                     </div>
                 </div>
             </div>
-        </div>
+        </div> <!--====  End of .row  ====-->
         <hr>
         <div class="row">
-            <div class="medium-4 columns">
+            <div class="medium-6 columns">
                 <div class="media-object">
                     <div class="media-object-section">
-                        <h5><a href="#">Subhead</a></h5>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est at, libero asperiores, itaque fuga sed dicta quos modi excepturi eligendi laborum nesciunt voluptatem odio. Nihil commodi voluptates eaque, repellendus odit?</p>
+                        <h5>Job Opportunities</h5>
+                        <p>
+                            <a href="http://www.elsegundo.org/depts/human_resources/job_opportunities.asp" target="_blank">Positions Available</br></a>
+                            <a href="http://www.elsegundo.org/depts/human_resources/part_time_opportunities.asp" target="_blank">Part-Time Opportunities</br></a>
+                            <a href="http://www.elsegundo.org/depts/human_resources/promotional_opportunities.asp" target="_blank">Promotional Opportunities</br></a>
+                        </p>
                     </div>
                 </div>
             </div>
-            <div class="medium-4 columns">
+            <div class="medium-6 columns">
                 <div class="media-object">
                     <div class="media-object-section">
-                        <h5><a href="#">Subhead</a></h5>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est at, libero asperiores, itaque fuga sed dicta quos modi excepturi eligendi laborum nesciunt voluptatem odio. Nihil commodi voluptates eaque, repellendus odit?</p>
+                        <h5>Mission</h5>
+                        <p>
+                            The Human Resources Department is responsible for providing all customers with excellent personnel services consistent with modern organizational principles and industry standards and for attracting and retaining a qualified, diverse, and safe work force while encouraging positive and harmonious working relationships.
+                        </p>
                     </div>
                 </div>
             </div>
-            <div class="medium-4 columns">
+        </div> <!--====  End of .row  ====-->
+        <hr>
+        <div class="row">
+            <div class="medium-12 columns">
                 <div class="media-object">
                     <div class="media-object-section">
-                        <h5><a href="#">Subhead</a></h5>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est at, libero asperiores, itaque fuga sed dicta quos modi excepturi eligendi laborum nesciunt voluptatem odio. Nihil commodi voluptates eaque, repellendus odit?</p>
+                        <h5>Frequently Asked Questions</h5>
+                        <p>
+                            <span style='font-weight: bold;'>What online application system does the City of El Segundo use?</span></br>
+                            • The City of El Segundo uses NEOGOV for accepting online applications.
+                        </p>
+                        <p>
+                            <span style='font-weight: bold;'>What do I need to do to apply online?</span></br>
+                            • Setting up a governmentjobs.com account is free. In order to set-up a free governmentjobs.com account, you only need an e-mail address. A number of e-mail providers, such as yahoo, g-mail, hotmail, etc. allow you to create an e-mail address for free in just a few minutes.
+                        </p>
+                        <p>
+                           <span style='font-weight: bold;'>Once I apply, who will see my application materials?</span></br>
+                           • Your application materials may only be viewed by City of El Segundo Human Resources Department staff and other City staff designated by the Human Resources Department to review application materials. Other agencies who use NEOGOV do not have access to your application materials for a position with the City of El Segundo.
+                        </p>
+                        <p>
+                           <span style='font-weight: bold;'>Where can I access a public computer to complete my application?</span></br>
+                           • The El Segundo Public Library is located just up the street from City Hall at 111 W. Mariposa Ave., El Segundo 90245. The library is open Monday through Thursday, 10:00 a.m. - 8:00 p.m.; Saturday 10:00 a.m. - 5:00 p.m. and Closed Friday and Sunday. Computers with internet are available for public use between normal business hours. You may also locate a public library near your home – most public libraries provide free internet services during hours they are open to the public.
+                        </p>
                     </div>
                 </div>
             </div>
-            <div class="medium-4 columns">
-                <div class="media-object">
-                    <div class="media-object-section">
-                        <h5><a href="#">Subhead</a></h5>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est at, libero asperiores, itaque fuga sed dicta quos modi excepturi eligendi laborum nesciunt voluptatem odio. Nihil commodi voluptates eaque, repellendus odit?</p>
-                    </div>
-                </div>
-            </div>
-            <div class="medium-4 columns">
-                <div class="media-object">
-                    <div class="media-object-section">
-                        <h5><a href="#">Subhead</a></h5>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est at, libero asperiores, itaque fuga sed dicta quos modi excepturi eligendi laborum nesciunt voluptatem odio. Nihil commodi voluptates eaque, repellendus odit?</p>
-                    </div>
-                </div>
-            </div>
-            <div class="medium-4 columns">
-                <div class="media-object">
-                    <div class="media-object-section">
-                        <h5><a href="#">Subhead</a></h5>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est at, libero asperiores, itaque fuga sed dicta quos modi excepturi eligendi laborum nesciunt voluptatem odio. Nihil commodi voluptates eaque, repellendus odit?</p>
-                    </div>
-                </div>
-            </div>
-        </div>
+        </div> <!--====  End of .row  ====-->
 
     <?php get_footer(); ?>
