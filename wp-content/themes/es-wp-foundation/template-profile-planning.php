ecb7708cbd97e414cf9e6e0e790f82f750705271
Jay Johnson <jayevanjohnson@gmail.com>
2016-06-03 13:55:24 -0700

    Add live content to planning & building template

--- wp-content/themes/es-wp-foundation/template-profile-planning.php
+++ wp-content/themes/es-wp-foundation/template-profile-planning.php
@@ -12,70 +12,105 @@
                 <h2 class="fontglyph inactive">I</h2>
             </div>
             <div class="medium-11 columns">
-                <h3 class="title-down">Planning &amp; Building Safety: <span style='font-weight: normal;'>Subhead</span></h3>
+                <h3 class="title-down">Planning &amp; Building Safety</span></h3>
             </div>
-        </div>
+        </div> <!--====  End of .rov  ====-->
         <hr>
         <div class="row">
             <div class="medium-12 columns">
                 <div class="media-object">
                     <div class="media-object-section">
-                        <h5><a href="#">Contact</a></h5>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est at, libero asperiores, itaque fuga sed dicta quos modi excepturi eligendi laborum nesciunt voluptatem odio. Nihil commodi voluptates eaque, repellendus odit?</p>
+                        <h5>Contact</h5>
+                        <p>
+                            <a href="mailto:slee@elsegundo.org?Subject=Hello%20Sam" target="_top">slee@elsegundo.org</a></br>
+                            <a href="tel:+1-310-524-2345">(310) 524-2345</a></br>
+                            350 Main Street, El Segundo, CA 90245</br>
+                            Monday - Thursday 7:00 a.m. - 6:00 p.m. | closed on Friday</br>
+                        </p>
                     </div>
                 </div>
             </div>
-        </div>
+        </div> <!--====  End of .row  ====-->
         <hr>
         <div class="row">
-            <div class="medium-4 columns">
+            <div class="medium-6 columns">
                 <div class="media-object">
                     <div class="media-object-section">
-                        <h5><a href="#">Subhead</a></h5>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est at, libero asperiores, itaque fuga sed dicta quos modi excepturi eligendi laborum nesciunt voluptatem odio. Nihil commodi voluptates eaque, repellendus odit?</p>
+                        <h5>Divisions</h5>
+                        <p>
+                            <a href="http://www.elsegundo.org/depts/planningsafety/buildingsafety/default.asp" target="_blank">Building Safety: Permits &amp; Inspections</a></br>
+                            <a href="http://www.elsegundo.org/depts/planningsafety/planning/default.asp" target="_blank">Planning: Zoning Information, Environmental Review &amp; Use Permits</a></br>
+                            <a href="http://www.elsegundo.org/depts/planningsafety/residential_sound_insulation_program/default.asp" target="_blank">Residential: Sound Insulation Program</a></br>
+
+                        </p>
+                        <hr>
+                        <h5>Services</h5>
+                        <p>
+                            <a href="http://www.elsegundo.org/depts/planningsafety/apc___appointment_plan_check/default.asp" target="_blank">Appointment Plan Check</a></br>
+                            <a href="http://www.sterlingcodifiers.com/codebook/index.php?book_id=587" target="_blank">Municipal Code</a></br>
+                            <a href="http://munipermits.elsegundo.org/" target="_blank">Online Municipal Permit Status</a></br>
+                            <a href="http://www.elsegundo.org/depts/planningsafety/planning/case_planner_process.asp" target="_blank">Case Planner Process</a></br>
+                        </p>
                     </div>
                 </div>
             </div>
-            <div class="medium-4 columns">
+            <div class="medium-6 columns">
                 <div class="media-object">
                     <div class="media-object-section">
-                        <h5><a href="#">Subhead</a></h5>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est at, libero asperiores, itaque fuga sed dicta quos modi excepturi eligendi laborum nesciunt voluptatem odio. Nihil commodi voluptates eaque, repellendus odit?</p>
+                        <h5>Mission</h5>
+                        <p>
+                            The Building Division ensures the health and welfare of the residents of El Segundo by regulating the construction, use, and occupancy of buildings. It continues a tradition of codes that can be traced through recorded history for over 4,000 years in which people have become increasingly aware of their ability to avoid the catastrophic consequences of building construction failure.  Today's codes are possible because of efforts and the collaboration of building code officials and key segments of the construction industry. The El Segundo Building and Safety Division ensures that the standards set by these codes for construction are met through plan review, permit issuance and on-site inspections.
+                        </p>
                     </div>
                 </div>
             </div>
-            <div class="medium-4 columns">
+        </div><!--====  End of .row  ====-->
+        <hr>
+        <div class="row">
+            <div class="medium-3 columns">
                 <div class="media-object">
                     <div class="media-object-section">
-                        <h5><a href="#">Subhead</a></h5>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est at, libero asperiores, itaque fuga sed dicta quos modi excepturi eligendi laborum nesciunt voluptatem odio. Nihil commodi voluptates eaque, repellendus odit?</p>
+                        <h5>Director: </br><span style='font-weight: normal;'>Sam Lee</span></h5>
+                        <p>
+                            <a href="mailto:slee@elsegundo.org?Subject=Hello%20Sam" target="_top">slee@elsegundo.org</a></br>
+                            <a href="tel:+1-310-524-2345">(310) 524-2345</a></br>
+                        </p>
                     </div>
                 </div>
             </div>
-            <div class="medium-4 columns">
+            <div class="medium-3 columns">
                 <div class="media-object">
                     <div class="media-object-section">
-                        <h5><a href="#">Subhead</a></h5>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est at, libero asperiores, itaque fuga sed dicta quos modi excepturi eligendi laborum nesciunt voluptatem odio. Nihil commodi voluptates eaque, repellendus odit?</p>
+                        <h5>Sr. Plancheck Engineer: </br><span style='font-weight: normal;'>Susan Yuzuki</span></h5>
+                        <p>
+                            <a href="mailto:syuzuki@elsegundo.org?Subject=Hello%20Susan" target="_top">syuzuki@elsegundo.org</a></br>
+                            <a href="tel:+1-310-524-2347">(310) 524-2347</a></br>
+                        </p>
                     </div>
                 </div>
             </div>
-            <div class="medium-4 columns">
+            <div class="medium-3 columns">
                 <div class="media-object">
                     <div class="media-object-section">
-                        <h5><a href="#">Subhead</a></h5>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est at, libero asperiores, itaque fuga sed dicta quos modi excepturi eligendi laborum nesciunt voluptatem odio. Nihil commodi voluptates eaque, repellendus odit?</p>
+                        <h5>Sr. Plancheck Engineer: </br><span style='font-weight: normal;'>Ocean Kwon</span></h5>
+                        <p>
+                            <a href="mailto:lquach@elsegundo.org?Subject=Hello%20Ocean" target="_top">lquach@elsegundo.org</a></br>
+                            <a href="tel:+1-310-524-2302">(310) 524-2302</a></br>
+                        </p>
                     </div>
                 </div>
             </div>
-            <div class="medium-4 columns">
+            <div class="medium-3 columns">
                 <div class="media-object">
                     <div class="media-object-section">
-                        <h5><a href="#">Subhead</a></h5>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est at, libero asperiores, itaque fuga sed dicta quos modi excepturi eligendi laborum nesciunt voluptatem odio. Nihil commodi voluptates eaque, repellendus odit?</p>
+                        <h5>Sr. Building Inspector: </br><span style='font-weight: normal;'>Jamie Taylor</span></h5>
+                        <p>
+                            <a href="mailto:jtaylor@elsegundo.org?Subject=Hello%20Jamie" target="_top">jtaylor@elsegundo.org</a></br>
+                            <a href="tel:+1-310-524-2350">(310) 524-2350</a></br>
+                        </p>
                     </div>
                 </div>
             </div>
-        </div>
+        </div><!--====  End of .row  ====-->
 
     <?php get_footer(); ?>
