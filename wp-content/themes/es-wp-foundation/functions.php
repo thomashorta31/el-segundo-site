97566cd4c388d5e774109a72abaaa071f86f3954
Sean Villalta <seanvillalta@gmail.com>
2016-06-03 17:01:07 -0700

    Revert styles and scripts paths in functions.php

--- wp-content/themes/es-wp-foundation/functions.php
+++ wp-content/themes/es-wp-foundation/functions.php
@@ -1,6 +1,6 @@
 <?php
 function register_my_styles(){
-  wp_register_style('foundation', get_template_directory_uri()."/assets/dest/css/bundle.min.css",array(),'5.1.1','screen');
+  wp_register_style('foundation', get_template_directory_uri()."/css/foundation-test.css",array(),'5.1.1','screen');
   wp_register_style('main', get_template_directory_uri()."/style.css",array(),'2.7','all');
 
   wp_enqueue_style(array('foundation','main'));
@@ -9,11 +9,10 @@ function register_my_styles(){
 add_action('wp_enqueue_scripts', 'register_my_styles');
 
 function register_my_scripts() {
-  // wp_register_script('jquery', "/js/vendor/jquery.js", array(),'2.1.0',false);
-  // wp_register_script('foundation-js', "/js/vendor/foundation.js", array(),'2.1.0',false);
-  wp_register_script('jquery', "/assets/dest/js/vendors/vendors.min.js", array(),'2.1.0',false);
+  wp_register_script('jquery', "/js/vendor/jquery.js", array(),'2.1.0',false);
+  wp_register_script('foundation-js', "/js/vendor/foundation.js", array(),'2.1.0',false);
 
-  wp_enqueue_script(array('jquery'));
+  wp_enqueue_script(array('jquery','foundation-js'));
 }
 add_action('wp_enqueue_scripts', 'register_my_scripts');
 
