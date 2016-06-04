0ba8ad4c21777fc3264cd3135cc6997b4d9a893a
Jay Johnson <jayevanjohnson@gmail.com>
2016-06-03 18:52:42 -0700

    Add live content to recreation & parks template

--- wp-content/themes/es-wp-foundation/template-profile-recreation-parks.php
+++ wp-content/themes/es-wp-foundation/template-profile-recreation-parks.php
@@ -12,70 +12,129 @@
                 <h2 class="fontglyph inactive">L</h2>
             </div>
             <div class="medium-11 columns">
-                <h3 class="title-down">Recreation &amp; Parks: <span style='font-weight: normal;'>Subhead</span></h3>
+                <h3 class="title-down">Recreation &amp; Parks</h3>
             </div>
-        </div>
-        <hr>
-        <div class="row">
-            <div class="medium-12 columns">
-                <div class="media-object">
-                    <div class="media-object-section">
-                        <h5><a href="#">Contact</a></h5>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est at, libero asperiores, itaque fuga sed dicta quos modi excepturi eligendi laborum nesciunt voluptatem odio. Nihil commodi voluptates eaque, repellendus odit?</p>
-                    </div>
-                </div>
-            </div>
-        </div>
+        </div> <!--====  End of .row  ====-->
         <hr>
         <div class="row">
             <div class="medium-4 columns">
                 <div class="media-object">
                     <div class="media-object-section">
-                        <h5><a href="#">Subhead</a></h5>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est at, libero asperiores, itaque fuga sed dicta quos modi excepturi eligendi laborum nesciunt voluptatem odio. Nihil commodi voluptates eaque, repellendus odit?</p>
+                        <h5>Checkout Building</h5>
+                        <p>
+                            Park, Picnic, Field &amp; Court Resevations</br>
+                            <a href="tel:+1-310-524-2700">(310) 524-2700</a></br>
+                            401 Sheldon Street, El Segundo, CA 90245</br>
+                        </p>
                     </div>
                 </div>
             </div>
             <div class="medium-4 columns">
                 <div class="media-object">
                     <div class="media-object-section">
-                        <h5><a href="#">Subhead</a></h5>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est at, libero asperiores, itaque fuga sed dicta quos modi excepturi eligendi laborum nesciunt voluptatem odio. Nihil commodi voluptates eaque, repellendus odit?</p>
+                        <h5>Gordon Clubhouse</h5>
+                        <p>
+                            Class Registration &amp; Cultural Arts</br>
+                            <a href="tel:+1-310-524-2362">(310) 524-2362</a></br>
+                            300 E. Pine Avenue, El Segundo, CA 90245</br>
+                        </p>
                     </div>
                 </div>
             </div>
             <div class="medium-4 columns">
                 <div class="media-object">
                     <div class="media-object-section">
-                        <h5><a href="#">Subhead</a></h5>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est at, libero asperiores, itaque fuga sed dicta quos modi excepturi eligendi laborum nesciunt voluptatem odio. Nihil commodi voluptates eaque, repellendus odit?</p>
+                        <h5>Joslyn Center</h5>
+                        <p>
+                            Seniors, Shuttle Services &amp; Transportation</br>
+                            <a href="tel:+1-310-524-2362">(310) 524-2362</a></br>
+                            339 Sheldon Street, El Segundo, CA 90245</br>
+                        </p>
                     </div>
                 </div>
             </div>
+        </div> <!--====  End of .row  ====-->
+        <hr>
+        <div class="row">
             <div class="medium-4 columns">
                 <div class="media-object">
                     <div class="media-object-section">
-                        <h5><a href="#">Subhead</a></h5>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est at, libero asperiores, itaque fuga sed dicta quos modi excepturi eligendi laborum nesciunt voluptatem odio. Nihil commodi voluptates eaque, repellendus odit?</p>
+                        <h5>Programs</h5>
+                        <p>
+                            <a href="http://www.elsegundo.org/depts/recreation/brochure_and_programs/aquatics.asp" target="_blank">Aquatics</br></a>
+                            <a href="http://www.elsegundo.org/depts/recreation/brochure_and_programs/arts_crafts_n_leisure.asp" target="_blank">Arts &amp; Crafts</br></a>
+                            <a href="http://www.elsegundo.org/depts/recreation/brochure_and_programs/dance.asp" target="_blank">Dance &amp; Music</br></a>
+                            <a href="http://www.elsegundo.org/depts/recreation/brochure_and_programs/exercise_n_fitness.asp" target="_blank">Exercise &amp; Fitness</br></a>
+                            <a href="http://www.elsegundo.org/depts/recreation/brochure_and_programs/senior_programs_n_trips.asp" target="_blank">Seniors</br></a>
+                            <a href="http://www.elsegundo.org/depts/recreation/recreation_facilities/youth_skate_park.asp" target="_blank">Skatepark</br></a>
+                            <a href="http://www.elsegundo.org/depts/recreation/brochure_and_programs/sports/default.asp" target="_blank">Sports</br></a>
+                        </p>
                     </div>
                 </div>
             </div>
-            <div class="medium-4 columns">
+            <div class="medium-8 columns">
                 <div class="media-object">
                     <div class="media-object-section">
-                        <h5><a href="#">Subhead</a></h5>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est at, libero asperiores, itaque fuga sed dicta quos modi excepturi eligendi laborum nesciunt voluptatem odio. Nihil commodi voluptates eaque, repellendus odit?</p>
+                        <h5>Documents &amp; Forms</h5>
+                        <p>
+                            <a href="http://www.elsegundo.org/civicax/filebank/blobdload.aspx?blobid=8468" target="_blank">Athletic Field &amp; Court Reservation Form</br></a>
+                            <a href="http://www.elsegundo.org/civicax/filebank/blobdload.aspx?blobid=9550" target="_blank">Community Center Reservation Form</br></a>
+                            <a href="http://www.elsegundo.org/civicax/filebank/blobdload.aspx?blobid=9551" target="_blank">Facility Permit Fees</br></a>
+                            <a href="http://www.elsegundo.org/civicax/filebank/blobdload.aspx?blobid=9548" target="_blank">Hilltop Park/Acacia Park Reservation Form (Resident Only)</br></a>
+                            <a href="http://www.elsegundo.org/civicax/filebank/blobdload.aspx?blobid=8903" target="_blank">Recreation Park Map</br></a>
+                            <a href="http://www.elsegundo.org/civicax/filebank/blobdload.aspx?blobid=4862" target="_blank">Recreation Park Picnic Reservation Form (Resident and Non-Resident)</br></a>
+                            <a href="http://www.elsegundo.org/civicax/filebank/blobdload.aspx?BlobID=9686" target="_blank">Special Event Use Permit Application (City Property Only)</br></a>
+                        </p>
                     </div>
                 </div>
             </div>
-            <div class="medium-4 columns">
+        </div> <!--====  End of .row  ====-->
+        <hr>
+        <div class="row">
+            <div class="medium-3 columns">
                 <div class="media-object">
                     <div class="media-object-section">
-                        <h5><a href="#">Subhead</a></h5>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est at, libero asperiores, itaque fuga sed dicta quos modi excepturi eligendi laborum nesciunt voluptatem odio. Nihil commodi voluptates eaque, repellendus odit?</p>
+                        <h5>Director: </br><span style='font-weight: normal;'>Meredith Petit</span></h5>
+                        <p>
+                            <a href="mailto:mpetit@elsegundo.org?Subject=Hello%20Meredith" target="_top">mpetit@elsegundo.org</a></br>
+                            <a href="tel:+1-310-524-2880">(310) 524-2880</a></br>
+                        </p>
                     </div>
                 </div>
             </div>
-        </div>
+            <div class="medium-3 columns">
+                <div class="media-object">
+                    <div class="media-object-section">
+                        <h5>Parks Supervisor: </br><span style='font-weight: normal;'>Joseph Casillas</span></h5>
+                        <p>
+                            <a href="mailto:jcasillas@elsegundo.org?Subject=Hello%20Joseph" target="_top">jcasillas@elsegundo.org</a></br>
+                            <a href="tel:+1-310-524-2894">(310) 524-2894</a></br>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="medium-3 columns">
+                <div class="media-object">
+                    <div class="media-object-section">
+                        <h5>Manager El Segundo TV: </br><span style='font-weight: normal;'>Dan O'Toole</span></h5>
+                        <p>
+                            <a href="mailto:dotoole@elsegundo.org?Subject=Hello%20Dan" target="_top">dotoole@elsegundo.org</a></br>
+                            <a href="tel:+1-310-524-2719">(310) 524-2719</a></br>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="medium-3 columns">
+                <div class="media-object">
+                    <div class="media-object-section">
+                        <h5>Recreation Supervisor: </br><span style='font-weight: normal;'>Jesse Bobbett</span></h5>
+                        <p>
+                            <a href="mailto:jbobbett@elsegundo.org?Subject=Hello%20Jesse" target="_top">jbobbett@elsegundo.org</a></br>
+                            <a href="tel:+1-310-524-2251">(310) 524-2251</a></br>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div><!--====  End of .row  ====-->
 
     <?php get_footer(); ?>
