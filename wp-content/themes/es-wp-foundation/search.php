98388602ab56e3faecad4917c72c17fe2fe64544
Jay Johnson <jayevanjohnson@gmail.com>
2016-06-04 19:55:40 -0700

    Update searchform.php, search.php and search field placeholder

--- wp-content/themes/es-wp-foundation/search.php
+++ wp-content/themes/es-wp-foundation/search.php
@@ -3,20 +3,18 @@
 Template Name: Search Page
 */
 ?>
-
 <?php get_header(); ?>
     <div class="callout large">
         <div class="row column text-center">
             <h1>Search Results</h1>
-            <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-            <p><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></p>
-            <?php endwhile; else : ?>
-                <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
-            <?php endif; ?>
         </div>
     </div>
-
-
-
-    <?php get_footer(); ?>
+    <div class="row text-center">
+        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+            <p><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></p>
+        <?php endwhile; else : ?>
+            <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+        <?php endif; ?>
+    </div>
+<?php get_footer(); ?>
 
--- wp-content/themes/es-wp-foundation/searchform.php
+++ wp-content/themes/es-wp-foundation/searchform.php
@@ -26,7 +26,7 @@
         <div class="large-12 columns">
             <div class="row collapse">
                 <div class="large-8 columns">
-                    <input type="text" name="s" id="s" placeholder="<?php esc_attr_e( 'What would you like to', 'foundation' ); ?>" />
+                    <input type="text" name="s" id="s" placeholder="<?php esc_attr_e( 'Search elsegundo.org', 'foundation' ); ?>" />
                 </div>
                 <div class="large-4 columns search-button">
                     <input type="submit" class="button prefix" name="submit" id="searchsubmit" value="<?php esc_attr_e( 'Search', 'foundation' ); ?>" />
