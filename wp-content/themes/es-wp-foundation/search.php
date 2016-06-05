cd01c034f6f91e1323d10053cd33eb798c857b05
Jay Johnson <jayevanjohnson@gmail.com>
2016-06-04 18:24:12 -0700

    Match style of search results page with established site style

--- wp-content/themes/es-wp-foundation/search.php
+++ wp-content/themes/es-wp-foundation/search.php
@@ -5,17 +5,16 @@ Template Name: Search Page
 ?>
 
 <?php get_header(); ?>
-
-        <div class="row">
-            <div class="medium-12 columns">
-                <h2>Search Results</h2>
-                <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-                <p><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></p>
-                <?php endwhile; else : ?>
-                    <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
-                <?php endif; ?>
-            </div>
+    <div class="callout large">
+        <div class="row column text-center">
+            <h1>Search Results</h1>
+            <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+            <p><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></p>
+            <?php endwhile; else : ?>
+                <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+            <?php endif; ?>
         </div>
+    </div>
 
 
 
